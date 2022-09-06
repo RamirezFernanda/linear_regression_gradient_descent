666f43efd00925c852a0a856d684e01a3d30b85b
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-09-05 22:51:45 -0500

    Update gradient_descent

--- GradientDescent/gradient_descent.py
+++ GradientDescent/gradient_descent.py
@@ -32,18 +32,24 @@ def linear_regression(x, m, b):
 
 def gradient_descent(x, y, m, b, learning_rate, epochs):
     predict = linear_regression(x, m, b)
-    cost = (1/x.shape[0]) * (np.sum(((y - predict)**2)))
-    new_cost = np.zeros(cost.shape)
+    prev_cost = 0
+    error = y - predict
     for i in range(1, epochs):
-        print(linear_regression(x, m, b))
         predict = linear_regression(x, m, b)
-        print(linear_regression(x, m, b))
+        cost = (1/x.shape[0]) * (np.sum(((y - predict)**2)))
         error = y - predict
-        if not np.equal(new_cost, cost):
-            deriv_m = (-2/x.shape[0]) * (x.T).dot(error)
-            deriv_b = (-2/x.shape[0]) * np.sum(error)
-            m = learning_rate * deriv_m
-            b = learning_rate * deriv_b
+
+        if not np.equal(prev_cost, cost):
+            prev_cost = cost
+
+            deriv_m = -(2/x.shape[0]) * (x.T).dot(error)
+
+            deriv_b = -(2/x.shape[0]) * np.sum(error)
+
+            m -= learning_rate * deriv_m
+
+            b -= learning_rate * deriv_b
+
         print(f'Epoch{i}')
     return m, b
 
@@ -54,7 +60,8 @@ x, y = read_dataset(
     'linear_regression_gradient_descent\DataSets\house_train.csv')
 m = np.random.rand((x.shape[1]))
 b = 0.5
+
 m, b = gradient_descent(x, y, m, b, learning_rate, epochs)
 print(f'y = {m} + {b}x')
-plt.plot(x, y, 'mo')
-# plt.show()
+#plt.plot(x, y, 'mo')
+plt.show()
