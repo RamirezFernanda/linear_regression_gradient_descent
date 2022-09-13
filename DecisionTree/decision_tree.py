3157d93e7fb4415b44e921723040495686221a3d
RamirezFernanda <ramirez.bmfernanda@gmail.com>
2022-09-12 22:48:01 -0500

    Update decision_tree

--- DecisionTree/decision_tree.py
+++ DecisionTree/decision_tree.py
@@ -8,6 +8,8 @@ df = pd.read_csv('../DataSets/winequality-red.csv')
 df['quality'] = df['quality'].replace(
     [3, 4, 5, 6, 7, 8], ['Mala', 'Mala', 'Regular', 'Regular', 'Buena', 'Buena'])
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 X = df.drop('quality', axis=1)
 y = df['quality']
 
@@ -18,10 +20,21 @@ dt = DecisionTreeClassifier(random_state=23)
 dt.fit(X_train, y_train)
 
 print(f'Exactitud del modelo: {dt.score(X_test, y_test)}')
+print('/------------------------------/')
 print(f'Exactitud del modelo (train): {dt.score(X_train, y_train)}')
+print('/------------------------------/')
 print(
-    f'Maximum distance between the root and any leaf = {DecisionTreeClassifier.get_depth(dt)}')
+    f'Distancia máxima entre la raíz y cualquier hoja = {DecisionTreeClassifier.get_depth(dt)}')
+print('/------------------------------/')
 # print(
 #    f'Parameters for this estimator. = {DecisionTreeClassifier.get_params(dt)}')
 print(
-    f'Number of leaves of the decision tree = {DecisionTreeClassifier.get_n_leaves(dt)}')
+    f'Número de hojas del arbol = {DecisionTreeClassifier.get_n_leaves(dt)}')
+print('/------------------------------/')
+print('/------------------------------/')
+print('/------------------------------/')
+
+predictions = dt.predict(X_test)
+pruebas = pd.DataFrame(
+    {'Valor esperado': y_test, 'Valor obtenido': predictions})
+print(f'Predicciones: {pruebas}')
